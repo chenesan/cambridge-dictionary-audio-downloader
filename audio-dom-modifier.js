b693a59ba39a4ba3b3bc4a3516301ca9930ab896
陳乙山 <pipio1994@gmail.com>
2019-08-04 11:42:23 +0800

    Fix downloaded file name

--- audio-dom-modifier.js
+++ audio-dom-modifier.js
@@ -1,10 +1,11 @@
 const audioEls = document.querySelectorAll('.sound.audio_play_button');
+const word = document.querySelector('.headword .hw').textContent;
 audioEls.forEach(audioEl => {
   const linkEl = document.createElement('a');
   linkEl.innerText = 'Download';
   const link = audioEl.getAttribute('data-src-mp3');
   linkEl.setAttribute('href', link);
-  linkEl.setAttribute('download', link);
+  linkEl.setAttribute('download', word + '.mp3');
   linkEl.classList.add('cadl-link');
   const parent = audioEl.parentNode;
   parent.insertBefore(linkEl, audioEl);
